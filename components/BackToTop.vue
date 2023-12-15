e59493cee00f028ed746cfc605a80d779e98118b
Lucas Yang <yangchenshin77@gmail.com>
2023-12-16 00:58:47 +0800

    Improve back to top button position

--- components/BackToTop.vue
+++ components/BackToTop.vue
@@ -1,20 +1,22 @@
 <template>
   <div
-    class="fixed right-[80px] bottom-[50px] transition-opacity duration-200 hidden md:block"
+    class="fixed inset-x-0 bottom-[50px] transition-opacity duration-200 pointer-events-none hidden md:block"
     :class="{
       'opacity-70 hover:opacity-100': show,
       'opacity-0': !show,
     }"
   >
-    <button
-      type="button"
-      class="flex justify-center items-center w-11 h-11 bg-red-500 rounded-lg"
-      @click="onClick"
-    >
-      <svg class="w-5 h-5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5 12.0834L10 7.08337L15 12.0834" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-      </svg>
-    </button>
+    <div class="container flex justify-end">
+      <button
+        type="button"
+        class="flex justify-center items-center w-11 h-11 bg-red-500 rounded-lg pointer-events-auto"
+        @click="onClick"
+      >
+        <svg class="w-5 h-5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5 12.0834L10 7.08337L15 12.0834" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
 
