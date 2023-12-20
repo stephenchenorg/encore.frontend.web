908603cee46cd1e95bc0215a2123a2189866acf8
stephen <tasb00429@gmail.com>
2023-12-20 16:33:30 +0800

    feat:  for demo purpose , aside-inner image is allow to click

--- pages/index.vue
+++ pages/index.vue
@@ -64,9 +64,9 @@
 
         <div id="aside" class="md:order-3">
           <div id="aside-inner" class="md:pb-4">
-            <!-- <button type="button" @click="openPostModal('2')"> -->
+             <button type="button" @click="openPostModal('ObjectId(612b553bad8487487ba24beb)')">
             <img class="rounded-lg" src="~/assets/example-images/encore-event-01.png">
-            <!-- </button> -->
+             </button>
           </div>
         </div>
 
